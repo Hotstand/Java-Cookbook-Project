0b99dbd35b107958e43cf0c9301cc07e172da039
layvong <145372097+layvong@users.noreply.github.com>
2023-11-27 14:02:03 -0600

    p

--- app/src/main/java/com/example/grocery_list/MainActivity.java
+++ app/src/main/java/com/example/grocery_list/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         mItemEdit = findViewById(R.id.item_editText);
         mAddButton = findViewById(R.id.add_button);
         mClearButton = findViewById(R.id.clear_button);
-        mGoToHomeButton = findViewById(R.id.go_to_home_button); // Initialize the new button
 
         List<ShoppingListItem> shoppingItems = new ArrayList<>();
         mAdapter = new ShoppingListAdapter(this, R.layout.list_item, shoppingItems);
@@ -59,23 +58,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Set up click listener for the "Go to Home Screen" button
-        mGoToHomeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Create an intent to navigate to HomeActivity
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivityForResult(intent, REQUEST_HOME_ACTIVITY); // Use startActivityForResult
-            }
-        });
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == REQUEST_HOME_ACTIVITY && resultCode == RESULT_OK) {
-            // Handle the result from HomeActivity, if needed
-        }
     }
 }
