8036de7900cb897bdfaae79bcf1c3dcaf8012d3b
layvong <145372097+layvong@users.noreply.github.com>
2023-11-28 07:26:49 -0600

    both add and clear buttons work. small ui issue where user cant easily see what they typed

--- app/src/main/java/com/example/javacookbookproject/GroceryList.java
+++ app/src/main/java/com/example/javacookbookproject/GroceryList.java
@@ -1,16 +1,15 @@
 package com.example.javacookbookproject;
 
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.javacookbookproject.databinding.FragmentGroceryListBinding;
 
@@ -20,25 +19,59 @@ import java.util.List;
 public class GroceryList extends Fragment {
 
     private FragmentGroceryListBinding binding;
+    private EditText itemEditText;
+    private List<ShoppingListItem> shoppingList;
+    private ShoppingListAdapter adapter;
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         binding = FragmentGroceryListBinding.inflate(inflater, container, false);
         return binding.getRoot();
-
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // add item to grocery list
-        binding.addButton.setOnClickListener(view1 -> {
-            binding.itemEditText.getText();
+        shoppingList = new ArrayList<>(); // This should be the list of your items
+
+        // Create the adapter and set it to the ListView
+        adapter = new ShoppingListAdapter(requireContext(), R.layout.list_item, shoppingList);
+        ListView listView = view.findViewById(R.id.shopping_listView);
+        listView.setAdapter(adapter);
+
+        // Get references to UI elements
+        itemEditText = view.findViewById(R.id.item_editText);
+        Button addButton = view.findViewById(R.id.add_button);
+        Button clearButton = view.findViewById(R.id.clear_button);
+
+        // Set click listener for the add button
+        addButton.setOnClickListener(v -> {
+            String itemName = itemEditText.getText().toString().trim();
+            if (!itemName.isEmpty()) {
+                // Create a new ShoppingListItem and add it to the list
+                ShoppingListItem newItem = new ShoppingListItem(itemName, false);
+                shoppingList.add(newItem);
+
+                // Notify the adapter that the data has changed
+                adapter.notifyDataSetChanged();
+
+                // Clear the EditText for the next item
+                itemEditText.getText().clear();
+            }
         });
 
+        // Set click listener for the clear button
+        clearButton.setOnClickListener(v -> {
+            // Clear the EditText
+            itemEditText.getText().clear();
+
+            // Clear the list of items
+            shoppingList.clear();
+
+            // Notify the adapter that the data has changed
+            adapter.notifyDataSetChanged();
+        });
     }
 
     @Override
@@ -46,5 +79,4 @@ public class GroceryList extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
 }
